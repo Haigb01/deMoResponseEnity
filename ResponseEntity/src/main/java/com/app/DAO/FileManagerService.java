c93d7eef689c15eaf16a5bae103fb90976c841d4
hai cao <haigb01@gmail.com>
2023-03-14 18:45:23 +0700

    add several methods

--- ResponseEntity/src/main/java/com/app/DAO/FileManagerService.java
+++ ResponseEntity/src/main/java/com/app/DAO/FileManagerService.java
@@ -1,7 +1,11 @@
 package com.app.DAO;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +22,49 @@ public class FileManagerService {
 	private Path getPath(String folder,String filename) {
 		File dir = Paths.get(app.getRealPath("/files/"),folder).toFile();
 		if(!dir.exists()){
-			//neu chua ton tai thi tao ra thu muc files
+			//neu chua to
 			dir.mkdir();
 		}
 		return Paths.get(dir.getAbsolutePath(), filename);
 	
 	}
 	public byte[] read(String folder,String filename) {
-		
+		Path path = this.getPath(folder, filename);
+		try {
+			return Files.readAllBytes(path);
+		}catch(IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	public List<String> save(String folder,MultipartFile[] files){
-		
+		List<String> filenames = new ArrayList<String>();
+		for(MultipartFile file: files) {
+			String name  = System.currentTimeMillis()+file.getOriginalFilename();
+			String filename = Integer.toHexString(name.hashCode())+ name.substring(name.lastIndexOf("."));
+			Path path = this.getPath(folder, filename);
+			try {
+				file.transferTo(path);
+				filenames.add(filename);
+				System.out.println(filenames);
+			}catch(Exception e ) {
+				e.printStackTrace();
+			}
+		}
+		return filenames;
 	}
 	public void delete(String folder, String filename) {
-		
+		Path path = this.getPath(folder, filename);
+		path.toFile().delete();
 	}
 	public List<String> list(String folder){
-		
+		List<String> filenames = new ArrayList<String>();
+		File dir = Paths.get(app.getRealPath("/files/"),folder).toFile();
+		if(dir.exists()) {
+			File[] files = dir.listFiles();
+			for(File file:files) {
+				filenames.add(file.getName());
+			}
+		}
+		return filenames;
 	}
 }
