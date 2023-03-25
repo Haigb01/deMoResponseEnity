599d01768aed6cad364214df921bbb426d3a2805
hai cao <haigb01@gmail.com>
2023-03-25 22:32:26 +0700

    security-loginForm() -> configuration and modify HomeController

--- ResponseEntity/src/main/java/com/app/Configuration/SecurityConfiguration.java
+++ ResponseEntity/src/main/java/com/app/Configuration/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,7 +19,7 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
+		 http
 //	        	.csrf().disable()
 //	        	.authorizeHttpRequests()
 //	            .requestMatchers("/").permitAll()
@@ -29,28 +31,44 @@ public class SecurityConfiguration {
 //	            .and().build();
 				// another way
 				.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
-					auth.requestMatchers("/").permitAll();
-					auth.requestMatchers("/user").hasRole("USER");
-					auth.requestMatchers("/admin").hasRole("ADMIN");
-				}).httpBasic(Customizer.withDefaults()).build();
-
+					auth.requestMatchers("/user/**").hasRole("USER");
+					auth.requestMatchers("/admin/**").hasRole("ADMIN");
+					auth.anyRequest().permitAll();
+				})
+//				.httpBasic(Customizer.withDefaults()).build();
+				.formLogin()
+				.loginPage("/login/form")//dia chi url
+				.loginProcessingUrl("/login")//action form login
+				.defaultSuccessUrl("/login/success",false)//trang sau khi dang nhap thanh cong
+				.failureUrl("/login/error")// dia chi url khi co loi
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/logout/success");
+			return http.build();
 	}
 	
 	@Bean
 	public InMemoryUserDetailsManager userDetailsManager() {
 		//create a user account
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
+		UserDetails user = User.withUsername("user")
+				.password(passwordEncoder().encode("password"))
 				.roles("USER")
 				.build();
 		//create a admin account
-		UserDetails admin = User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("password")
+		UserDetails admin = User.withUsername("admin")
+				.password(passwordEncoder().encode("password"))
 				.roles("ADMIN")
 				.build();
 		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- ResponseEntity/src/main/java/com/app/Security_demo/HomeController.java
+++ ResponseEntity/src/main/java/com/app/Security_demo/HomeController.java
@@ -1,19 +1,42 @@
 package com.app.Security_demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-	
 	@ResponseBody
 	@GetMapping("/")
 	public String home() {
-		
 		return "Hello World!";
 	} 
 	
+	@RequestMapping("/login/form")
+	public String form() {
+		return "login/login-form";
+	}
+	
+	@RequestMapping("/login/success")
+	public String login(Model model) {
+		model.addAttribute("message", "Success!");
+		return "forward:/login/form";
+	}
+	@RequestMapping("/login/error")
+	public String error(Model model) {
+		model.addAttribute("message", "ERROR!");
+		return "forward:/login/form";
+	}
+	
+	@RequestMapping("/logout/success")
+	public String logout(Model model) {
+		model.addAttribute("message","Logout success!");
+		return "forward:/login/form";
+	}
 	@ResponseBody
 	@GetMapping("/user")
 	public String user() {
